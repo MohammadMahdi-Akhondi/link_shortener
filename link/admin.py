db626877ee62cb6b645b0008e0e57ff2301a889f
MohammadMahdi Akhondi <mohammad.a8098@gmail.com>
2023-05-18 17:11:08 +0330

    Add Link model and register it

--- link/admin.py
+++ link/admin.py
@@ -1,3 +1,29 @@
+from django.utils.translation import gettext_lazy as _
 from django.contrib import admin
 
-# Register your models here.
+from .models import Link
+
+
+admin.site.site_header = _('Link Shortener Service')
+admin.site.site_title = _('Link Shortener Service')
+admin.site.index_title = _('Link Shortener Management')
+
+
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'user',
+        'real_link',
+    )
+    search_fields = (
+        'title',
+        'real_link',
+        'token',
+        'description',
+    )
+    list_filter = (
+        'is_active',
+        'expire_at',
+        'created_at'
+    )
--- link/models.py
+++ link/models.py
@@ -1,3 +1,59 @@
+from django.utils.translation import gettext_lazy as _
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Link(models.Model):
+    title = models.CharField(
+        max_length=80,
+        verbose_name=_('Title')
+    )
+    real_link = models.URLField(
+        verbose_name=_('Real link')
+    )
+    token = models.CharField(
+        max_length=10,
+        unique=True,
+        verbose_name=_('Token')
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='links',
+        verbose_name=_('User')
+    )
+    description = models.TextField(
+        null=True, blank=True,
+        verbose_name=_('Description')
+    )
+    clicks_count = models.IntegerField(
+        default=0,
+        verbose_name=_('Clicks count')
+    )
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name=_('Is active?')
+    )
+    expire_at = models.DateTimeField(
+        null=True, blank=True,
+        verbose_name=_('Expire at')
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_('Created')
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name=_('Updated')
+    )
+    deleted_at = models.DateTimeField(
+        null=True, blank=True,
+        verbose_name=_('Is deleted')
+    )
+
+    class Meta:
+        verbose_name = _('Link')
+        verbose_name_plural = _('Links')
+
+    def __str__(self):
+        return self.title
