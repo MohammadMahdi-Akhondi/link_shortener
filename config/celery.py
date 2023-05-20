2a84bd4cd40df47997b1b9cc1398680a95fe67c8
MohammadMahdi Akhondi <mohammad.a8098@gmail.com>
2023-05-20 13:08:33 +0330

    Add celery to project

---
+++ config/celery.py
@@ -0,0 +1,8 @@
+from celery import Celery
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+app = Celery('config')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
