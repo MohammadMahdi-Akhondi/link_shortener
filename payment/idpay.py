947f8f4fc54997e65cde4a3949407221b77ce10c
MohammadMahdi Akhondi <mohammad.a8098@gmail.com>
2023-05-22 19:01:17 +0330

    Add idpay.py to app payment

---
+++ payment/idpay.py
@@ -0,0 +1,45 @@
+import requests
+import json
+
+
+class APIException(Exception):
+    pass
+
+
+class HTTPException(Exception):
+    pass
+
+
+class IDPayAPI(object):
+    def __init__(self, apikey, sandbox):
+        self.version = 'v1.1/'
+        self.domain = 'api.idpay.ir/'
+        self.apikey = apikey
+        self.sandbox = sandbox
+        self.header = {
+            'Content-Type': 'application/json',
+            'X-API-KEY': apikey,
+            'X-SANDBOX': '1' if sandbox else '0',
+        }
+    
+    def __str__(self):
+        return f'IDPay.APIKEY={self.apikey}'
+
+    def _request(self, action='payment/', method='', data={}):
+        url = 'https://' + self.domain + self.version + action + method
+        try:
+            response = requests.post(url=url, headers=self.header, data=json.dumps(data))
+            error_status_codes = list(range(400, 406))
+            if response.status_code in error_status_codes:
+                print(response.json())
+                print(data)
+                print(self.header)
+                # raise APIException(f'status code: {response.status_code}, error code: {error_code}')
+
+            return response.json()
+
+        except requests.exceptions.RequestException as error:
+            raise HTTPException(error)
+    
+    def create_transaction(self, data=None) -> dict:
+        return self._request(action='payment', data=data)
